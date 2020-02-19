f4a5938e1400c2354d70df41cd4be6f8eb9cb161
MariaSobol <sablemari@gmail.com>
2020-02-19 22:12:19 +0300

    Проект с урока №2

--- js/main.js
+++ js/main.js
@@ -1,28 +1,50 @@
-const products = [
-    {id: 1, title: 'Notebook', price: 20000},
-    {id: 2, title: 'Mouse', price: 1500},
-    {id: 3, title: 'Keyboard', price: 5000},
-    {id: 4, title: 'Gamepad', price: 4500},
-];
+class ProductItem {
+    constructor(product, img = "http://placehold.it/180x200/AAE99C/ecf0f1") {
+        this.title = product.title;
+        this.price = product.price;
+        this.id = product.id;
+        this.img = img;
+    }
 
-//Используем деструктуризацию при передаче параметров в функцию
-//Устанавливаем значения по умолчанию
-//Убираем return и лищние фигурные скобки
-const renderProduct = ({title, price, img = "http://placehold.it/180x200/AAE99C/ecf0f1"}) => `<div class="product-item">
-                <img class="product-img" src="${img}" alt="${title}">
-                <div class="desc">
-                    <h3>${title}</h3>
-                    <p>${price} \u20bd</p>
-                    <button class="buy-btn">Купить</button>
-                </div>
-            </div>`;
+    render() {
+        return `<div class="product-item" data-id="${this.id}">
+                    <img class="product-img" src="${this.img}" alt="${this.title}">
+                    <div class="desc">
+                        <h3>${this.title}</h3>
+                        <p>${this.price} \u20bd</p>
+                        <button class="buy-btn">Купить</button>
+                    </div>
+                </div>`
+    }
+}
 
+class ProductList {
+    constructor(container = '.products') {
+        this.container = container;
+        this.goods = [];
+        this.allProducts = [];
+        this._fetchProducts();
+        this.render();
+    }
 
-const renderProducts = (list) => {
-    // const productList = list.map((item) => renderProduct(item));
-    // document.querySelector('.products').innerHTML = productList.join(''); //убираем запятые при помощи метода join()
-    // Используем insertAdjacentHTML вместо innerHTML
-    document.querySelector('.products').insertAdjacentHTML('beforeend', list.map((item) => renderProduct(item)).join(''));
-};
+    _fetchProducts() {
+        this.goods = [
+            {id: 1, title: 'Notebook', price: 1000},
+            {id: 2, title: 'Mouse', price: 100},
+            {id: 3, title: 'Keyboard', price: 250},
+            {id: 4, title: 'Gamepad', price: 150},
+        ];
+    }
 
-renderProducts(products);
+    render() {
+        const block = document.querySelector(this.container);
+
+        for (let product of this.goods) {
+            const productObject = new ProductItem(product);
+            this.allProducts.push(productObject);
+            block.insertAdjacentHTML('beforeend', productObject.render());
+        }
+    }
+}
+
+new ProductList();
