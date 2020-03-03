2408694294ea3dba720eacd4e544a6433dd1f89e
MariaSobol <sablemari@gmail.com>
2020-03-03 22:37:11 +0300

    Домашнее задание №5 (часть 2)

--- js/main.js
+++ js/main.js
@@ -4,11 +4,14 @@ const app = new Vue({
     el: '#app',
     data: {
         catalogUrl: '/catalogData.json',
+        cartUrl: '/getBasket.json',
         products: [],
         filtered: [],
         cart: [],
+        isVisibleCart: false,
         searchLine: '',
         imgCatalog: 'http://placehold.it/180x200/AAE99C/ecf0f1',
+        imgCart: 'http://placehold.it/50x80/AAE99C/ecf0f1',
     },
     methods: {
         getJson(url){
@@ -18,8 +21,40 @@ const app = new Vue({
                     console.log(error);
                 })
         },
+
         addProduct(product){
-            console.log(product.id_product);
+            this.getJson(`${API}/addToBasket.json`)
+                .then(data => {
+                    if(data.result === 1){
+                        let find = this.cart.find(el => el.id_product === product.id_product);
+                        if(find){
+                            find.quantity++;
+                        } else {
+                            //TODO: проверить, дописать
+                            product.quantity = 1;
+                            this.cart.push(product);// = [product]; //TODO: работает неправильно?
+                            console.log("не найден");
+                        }
+                    } else {
+                        alert('Error');
+                    }
+                })
+        },
+
+        removeProduct(product){
+            this.getJson(`${API}/deleteFromBasket.json`)
+                .then(data => {
+                    if(data.result === 1){
+                        let find = this.cart.find(el => el.id_product === product.id_product);
+                        if(find.quantity > 1){ // если товара > 1, то уменьшаем количество на 1
+                            find.quantity--;
+                        } else { // удаляем
+                            this.cart.splice(this.cart.indexOf(find), 1);
+                        }
+                    } else {
+                        alert('Error');
+                    }
+                })
         },
 
         filter(){
@@ -27,6 +62,7 @@ const app = new Vue({
             this.filtered = this.products.filter(product => regexp.test(product.product_name));
         }
     },
+
     // хук жизненного цикла
     mounted(){
         this.getJson(`${API + this.catalogUrl}`)
@@ -36,6 +72,12 @@ const app = new Vue({
                 }
             });
         this.filtered = this.products;
+        this.getJson(`${API + this.cartUrl}`)
+            .then(data => {
+                for(let el of data.contents){
+                    this.cart.push(el);
+                }
+            });
     }
 });
 
