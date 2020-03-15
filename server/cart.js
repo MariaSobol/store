24eab1dacf68f4e9a6d80a243ed1fcb5b223c309
MariaSobol <sablemari@gmail.com>
2020-03-16 00:34:06 +0300

    Домашнее задание №7 (удаление товара из корзины)

--- server/cart.js
+++ server/cart.js
@@ -8,7 +8,9 @@ const change = (cart, req) => {
     return JSON.stringify(cart, null, 4);
 };
 const del = (cart, req) => {
-    // homework
+    const find = cart.contents.find(el => el.id_product === +req.params.id);
+    cart.contents.splice(cart.contents.indexOf(find), 1);
+    return JSON.stringify(cart, null, 4);
 };
 
 module.exports = {
